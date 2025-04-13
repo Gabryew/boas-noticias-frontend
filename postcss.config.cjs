b38441f337ace28eb2b338eb4fd5bcc282a09f53
Gabryew <114834989+Gabryew@users.noreply.github.com>
2025-04-13 17:28:51 +0000

    Move Tailwind CSS, Autoprefixer, and PostCSS to dependencies for Vercel

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 // postcss.config.cjs
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    'tailwindcss': require('tailwindcss'),
+    'autoprefixer': require('autoprefixer'),
   },
 };
