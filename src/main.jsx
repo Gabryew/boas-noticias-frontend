e927884e70905a8a1a1b081ad3057c5020a51498
Gabryew <114834989+Gabryew@users.noreply.github.com>
2025-04-13 19:08:29 +0000

    Melhorando o filtro de notícias com spaCy para análise em português

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Certifique-se de que está importando 'react-dom/client'
-import App from './App'; // Certifique-se de que o App está sendo importado corretamente
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Crie o root com o id 'root'
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
