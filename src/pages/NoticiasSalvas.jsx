7292cb8050632063d1d63a730a7b99e3b3573822
Gabryew <114834989+Gabryew@users.noreply.github.com>
2025-04-15 00:19:33 +0000

    att

--- src/pages/NoticiasSalvas.jsx
+++ src/pages/NoticiasSalvas.jsx
@@ -11,14 +11,6 @@ export default function NoticiasSalvas() {
     setSalvas(noticiasSalvas);
   }, []);
 
-  if (salvas.length === 0) {
-    return (
-      <div className="flex items-center justify-center h-screen bg-black text-white">
-        <p className="text-lg">Você ainda não salvou nenhuma notícia.</p>
-      </div>
-    );
-  }
-
   return (
     <div className="w-screen bg-black text-white">
       {/* Menu superior */}
@@ -39,26 +31,32 @@ export default function NoticiasSalvas() {
         </div>
       </div>
 
-      <div className="p-6">
-        <h1 className="text-3xl font-bold mb-4">Notícias Salvas</h1>
-        {salvas.map((noticia, index) => (
-          <div
-            key={index}
-            className="mb-6 cursor-pointer"
-            onClick={() => navigate(`/noticia/${encodeURIComponent(noticia.link)}`)}
-          >
+      {salvas.length === 0 ? (
+        <div className="flex items-center justify-center h-screen bg-black text-white">
+          <p className="text-lg">Você ainda não salvou nenhuma notícia.</p>
+        </div>
+      ) : (
+        <div className="p-6">
+          <h1 className="text-3xl font-bold mb-4">Notícias Salvas</h1>
+          {salvas.map((noticia, index) => (
             <div
-              className="h-40 bg-cover bg-center rounded-xl mb-2"
-              style={{ backgroundImage: `url(${noticia.image || "default-image.jpg"})` }} // Definindo um fallback de imagem
-            />
-            <h2 className="text-xl font-semibold">{noticia.title}</h2>
-            <div className="text-sm text-gray-300">
-              <span>{noticia.source}</span>
-              {noticia.readingTime && <span> - Tempo de leitura: {noticia.readingTime}</span>}
+              key={index}
+              className="mb-6 cursor-pointer"
+              onClick={() => navigate(`/noticia/${encodeURIComponent(noticia.link)}`)}
+            >
+              <div
+                className="h-40 bg-cover bg-center rounded-xl mb-2"
+                style={{ backgroundImage: `url(${noticia.image || "default-image.jpg"})` }} // Definindo um fallback de imagem
+              />
+              <h2 className="text-xl font-semibold">{noticia.title}</h2>
+              <div className="text-sm text-gray-300">
+                <span>{noticia.source}</span>
+                {noticia.readingTime && <span> - Tempo de leitura: {noticia.readingTime}</span>}
+              </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
